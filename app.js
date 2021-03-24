af1b506ef268cded820fad22d86263e6d4e56b2f
koukai76 <61773788+koukai76@users.noreply.github.com>
2021-03-25 02:46:23 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+const { exec } = require('child_process');
+
+(async () => {
+  const url = 'https://iam.cloud.ibm.com/identity/token';
+
+  const params = new URLSearchParams();
+  params.append('grant_type', 'urn:ibm:params:oauth:grant-type:apikey');
+  params.append('apikey', process.env.OW_IAM_NAMESPACE_API_KEY);
+
+  const res = await axios.post(url, params);
+
+  exec(
+    `export OW_APIGW_ACCESS_TOKEN="${res.data['access_token']}"`,
+    (err, stdout, stderr) => {}
+  );
+})();
